b70932cfb247ddd038bbd32cb60294240c87f184
RoshanSawlani <roshansawlani@gmail.com>
2024-08-18 20:55:49 +0530

    Connected Mongo DB to Node JS / Express JS App

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,27 @@
 const express = require('express');
 const { chats } = require('./data/data');
 const dotenv = require('dotenv');
-const app = express();
-
+const connectDB = require('./config/db')
+const colors = require('colors')
 
 dotenv.config()
-app.get('/',(req,res)=>{
+connectDB()
+const app = express();
+
+app.get('/', (req, res) => {
     res.send("API is running");
 })
 
-app.get('/api/chat',(req,res)=>{
+app.get('/api/chat', (req, res) => {
     res.send(chats)
 })
 
-app.get('/api/chat/:id',(req,res)=>{
+app.get('/api/chat/:id', (req, res) => {
     // console.log(req.params.id)
-    const singleChat = chats.find(c=>c._id === req.params.id)
+    const singleChat = chats.find(c => c._id === req.params.id)
     res.send(singleChat)
 })
 
 const PORT = process.env.PORT || 8082;
 
-app.listen(PORT,console.log("server started on port 8082"));
+app.listen(PORT, console.log(`server started on PORT ${PORT}`.yellow.bold));
