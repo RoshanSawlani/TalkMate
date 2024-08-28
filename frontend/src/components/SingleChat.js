ce90060b20d64b23491ebdcf9584079ed8ad149d
RoshanSawlani <roshansawlani@gmail.com>
2024-08-28 19:26:21 +0530

    Added UpdateGroupChatModal with features to add and remove users, rename group chat, and integrated modal into SingleChat component

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -4,6 +4,7 @@ import { Box, IconButton, Text } from '@chakra-ui/react'
 import { ArrowBackIcon } from '@chakra-ui/icons'
 import { getSender,getSenderFull } from '../config/ChatLogics'
 import ProfileModal from './miscellaneous/ProfileModal'
+import UpdateGroupChatModal from './miscellaneous/UpdateGroupChatModal'
 
 const SingleChat = ({fetchAgain, setFetchAgain}) => {
     const {user,selectedChat,setSelectedChat} = ChatState()
@@ -19,10 +20,13 @@ const SingleChat = ({fetchAgain, setFetchAgain}) => {
                             <ProfileModal user={getSenderFull(user,selectedChat.users)}/>
                             </>
                         ) : (<>{selectedChat.chatName.toUpperCase()}
-                        {/* <UpdateGroupChatModal fetchAgain={fetchAgain} setFetchAgain={setFetchAgain}/> */}
+                        <UpdateGroupChatModal fetchAgain={fetchAgain} setFetchAgain={setFetchAgain}/>
                             </>)
                         }
                     </Text>
+                    <Box display="flex" flexDir="column" justifyContent="flex-end" p={3} bg='#E8E8E8' w="100%" h="100%" borderRadius="lg" overflowY="hidden">
+                        {/* Messages Here */}
+                    </Box>
                 </>
             ) : (
                 <Box display="flex" alignItems="center" justifyContent="center" h="100%">
